d6dbd1efe9eee37624ac52127b07b90d3bd87a9b
Victoria <dzadranik@gmail.com>
2020-01-23 13:57:09 +0300

    updated template

--- js/main.js
+++ js/main.js
@@ -1,57 +1,40 @@
+let aboutUsBtn = document.querySelector('.link-about-us');
+let aboutUsBlock = document.querySelector(".about-us");
+let contactUsBtn = document.querySelector('.link-contact-us');
+let contactUsBlock = document.querySelector(".contact-us");
+
+aboutUsBtn.addEventListener('click', function() {aboutUsBlock.scrollIntoView({ behavior: 'smooth' });});
+contactUsBtn.addEventListener('click', function() {contactUsBlock.scrollIntoView({ behavior: 'smooth' });});
+
 function initialize() {  
    
     var secheltLoc = new google.maps.LatLng  (34.080631, -117.576030);
-
-                var myMapOptions = {
-                    center: secheltLoc,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    zoom: 15,
-                    scrollwheel: false,
-                    mapTypeControl: false,
-                    zoomControl: false,
-                    styles: [
-                        {
-                            "featureType": "all",
-                            "elementType": "all",
-                            "stylers": [
-                                {
-                                    "hue": "#0000b0"
-                                },
-                                {
-                                    "invert_lightness": "true"
-                                },
-                                {
-                                    "saturation": -30
-                                }
-                            ]
-                        }
-                    ]
-                };
-                var theMap = new google.maps.Map(document.getElementById("map"), myMapOptions);
-               
-                var marker = new google.maps.Marker({
-                    map: theMap,
-                    icon: image,
-                    draggable: false,
-                    animation: google.maps.Animation.DROP,
-                    position: secheltLoc,
-                    visible: true
-                });
-
+    var myMapOptions = {
+        center: secheltLoc,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoom: 15,
+        scrollwheel: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        styles: [
+            {
+                "featureType": "all",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "hue": "#0000b0"
+                    },
+                    {
+                        "invert_lightness": "true"
+                    },
+                    {
+                        "saturation": -30
+                    }
+                ]
             }
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-
-
-let aboutUsBtn = document.querySelector('.header-link--about-us');
-let aboutUsBlock = document.querySelector(".about-us");
-let contactUsBtn = document.querySelector('.header-link--contact-us');
-let contactUsBlock = document.querySelector(".contact-us");
-
-const handleButtonClick = (showBlock) => {
-    showBlock.scrollIntoView({block: "start", behavior: "smooth"});
+        ]
+    };
+    var theMap = new google.maps.Map(document.getElementById("map"), myMapOptions);
+    
 }
-
-aboutUsBtn.addEventListener('click', () => handleButtonClick(aboutUsBlock));
-contactUsBtn.addEventListener('click', () => handleButtonClick(contactUsBlock));
+google.maps.event.addDomListener(window, 'load', initialize);
